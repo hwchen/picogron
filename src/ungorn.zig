da5e6fb8bc868f75c756d10f32e38a4b002f2192
Walther Chen <walther.chen@gmail.com>
2024-04-03 08:22:45 -0400

    ungorn: only print key when not array item

--- src/ungorn.zig
+++ src/ungorn.zig
@@ -16,22 +16,26 @@ pub fn ungorn(rdr: anytype, wtr: anytype) !void {
         var path_val_it = mem.splitBackwardsSequence(u8, line, " = ");
         const val = path_val_it.next().?;
         const path = path_val_it.next().?;
-        if (!mem.eql(u8, path, "json")) {
+        const val_is_obj = mem.eql(u8, val, "{}");
+        const val_is_arr = mem.eql(u8, val, "[]");
+        const path_is_arr = path[path.len - 1] == ']';
+        const path_is_nonroot = !mem.eql(u8, path, "json");
+        if (path_is_nonroot and !path_is_arr) {
             // assumes that fields do not contain periods
             var path_it = mem.splitBackwardsSequence(u8, path, ".");
             const key = path_it.next().?;
+            std.debug.print("path: {s}, key: {s}, is_arr{any}\n", .{ path, key, path_is_arr });
             try jws.objectField(key);
         }
-        if (mem.eql(u8, val, "{}")) {
+        if (val_is_obj) {
             try jws.beginObject();
-        } else if (mem.eql(u8, val, "[]")) {
+        } else if (val_is_arr) {
             try jws.beginArray();
         } else {
             try jws.write(val);
         }
-        // TODO when to end object or array?
         // Can track previous path to see if something was popped
-        if (path.len < prev_path.len) {
+        if (path.len < prev_path.len and !val_is_obj and !val_is_arr) {
             if (path[path.len - 1] == ']') {
                 try jws.endArray();
             } else {
