f3d1b020fb5b142b317dc1f0965ff3eff5b359ba
Walther Chen <walther.chen@gmail.com>
2024-04-08 06:16:56 -0400

    ungorn: debug flush checked at comptime

--- src/ungorn.zig
+++ src/ungorn.zig
@@ -43,10 +43,10 @@ pub fn ungorn(rdr: anytype, wtr: anytype) !void {
         }
         prev_path_nest = path_info.nest;
 
-        if (builtin.mode == .Debug) {
+        comptime if (builtin.mode == .Debug) {
             // flushing more often helps with debugging
             try bw.flush();
-        }
+        };
 
         // write fields and values
         if (last_field == .object or last_field == .object_in_brackets) {
@@ -67,10 +67,10 @@ pub fn ungorn(rdr: anytype, wtr: anytype) !void {
             // this also prints numbers, null, bools exactly as they were read.
             try jws.print("{s}", .{val});
         }
-        if (builtin.mode == .Debug) {
+        comptime if (builtin.mode == .Debug) {
             // flushing more often helps with debugging
             try bw.flush();
-        }
+        };
     }
 
     // Close any remaining objects or arrays
