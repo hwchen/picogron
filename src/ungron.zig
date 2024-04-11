e67de50587385b0aed74f7ae8b1be51b3a220bf9
Walther Chen <walther.chen@gmail.com>
2024-04-11 09:20:30 -0400

    move gron parsing to own file

--- src/ungron.zig
+++ src/ungron.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 const mem = std.mem;
 const math = std.math;
+const parse_gron = @import("parse_gron.zig");
 
 pub fn ungron(rdr: anytype, wtr: anytype) !void {
     var br = std.io.bufferedReaderSize(4096 * 8, rdr);
@@ -13,13 +14,13 @@ pub fn ungron(rdr: anytype, wtr: anytype) !void {
     // tracks nesting levels of array and object
     // Currently uses difference in nesting level between two paths to know
     // how far back to pop.
-    var stack = try std.BoundedArray(LastField, 1024).init(0);
+    var stack = try std.BoundedArray(parse_gron.LastField, 1024).init(0);
     try stack.append(.root);
 
     var prev_path_nest: u32 = 0;
     var line_buf: [4096 * 1000]u8 = undefined;
     while (try input.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
-        const path_info = parsePath(line);
+        const path_info = parse_gron.parseLine(line);
         const val = path_info.value;
         const val_is_obj = val[0] == '{';
         const val_is_arr = val[0] == '[';
@@ -76,85 +77,4 @@ pub fn ungron(rdr: anytype, wtr: anytype) !void {
     }
     _ = try bw.write("\n");
     try bw.flush();
-}
-
-const PathInfo = struct {
-    nest: u32,
-    last_field: LastField,
-    // Only needed immediately after parsing. Lives as long
-    // as a line
-    last_field_str: []const u8,
-    value: []const u8,
-};
-
-const LastField = enum {
-    root,
-    array,
-    object,
-    object_in_brackets,
-};
-
-// simple parsing
-fn parsePath(line: []const u8) PathInfo {
-    std.debug.assert(mem.eql(u8, line[0..4], "json"));
-    var last_field: LastField = .root;
-    var nest: u32 = 0;
-    var last_field_start: usize = 0;
-    var path_end: usize = 0;
-    var i: usize = 4;
-    while (i < line.len) {
-        const c = line[i];
-        switch (c) {
-            ' ' => {
-                path_end = i;
-                break;
-            },
-            '.' => {
-                last_field = .object;
-                last_field_start = i;
-                nest += 1;
-            },
-            '[' => {
-                if (line[i + 1] == '"') {
-                    last_field = .object_in_brackets;
-                    last_field_start = i + 1;
-                    i = endOfBracketedField(line, i + 1);
-                } else {
-                    last_field = .array;
-                    last_field_start = i;
-                }
-                nest += 1;
-            },
-            else => {},
-        }
-        i += 1;
-    }
-    const last_field_str = switch (last_field) {
-        .root => &.{},
-        .array => &.{},
-        .object => line[last_field_start + 1 .. path_end],
-        // remove ]
-        .object_in_brackets => line[last_field_start .. path_end - 1],
-    };
-    return PathInfo{
-        .nest = nest,
-        .last_field = last_field,
-        .last_field_str = last_field_str,
-        .value = line[path_end + 3 .. line.len - 1], // removes leading `=` and semicolon
-    };
-}
-
-// returns index of last bracket of field-in-bracket syntax
-fn endOfBracketedField(line: []const u8, start_idx: usize) usize {
-    var i: usize = start_idx;
-    while (i < line.len) {
-        switch (line[i]) {
-            '\\' => i += 1,
-            ']' => break,
-            else => {},
-        }
-        i += 1;
-    }
-    std.debug.assert(line[i] == ']');
-    return i;
 }
