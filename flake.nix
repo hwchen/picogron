cf34566b7022d34a39fd425cae0ae5c6ce8c6320
Walther Chen <walther.chen@gmail.com>
2024-04-12 17:14:10 -0400

    nix: build and run

--- flake.nix
+++ flake.nix
@@ -27,16 +27,51 @@
         overlays = [
           (zig-overlay.overlays.default)
         ];
+      };
+      nativeBuildInputs = [pkgs.zigpkgs.master-2024-04-07];
+      picogron = pkgs.stdenv.mkDerivation {
+        pname = "picogron";
+        version = "v0.1.0";
+        src = ./.;
+        nativeBuildInputs = nativeBuildInputs;
+        buildPhase = ''
+          mkdir -p .cache
+          ln -s ${pkgs.callPackage ./deps.nix { }} .cache/p
+          zig build install --cache-dir $(pwd)/zig-cache --global-cache-dir $(pwd)/.cache -Dcpu=native -Doptimize=ReleaseFast --prefix $out
+        '';
+        meta = with pkgs.lib; {
+          description = "Smaller and faster gron";
+          homepage = "https://github.com/hwchen/picogron";
+          license = licenses.mit;
+          mainProgram = "picogron";
+        };
+      };
+
+      zls = zls-flake.packages.${system}.zls;
+      lib = pkgs.lib;
+      in {
+        # nix build
+        packages = {
+          inherit picogron;
+          default = picogron;
         };
 
-        zls = zls-flake.packages.${system}.zls;
-
-        lib = pkgs.lib;
+        # nix run
+        apps = let
+          picogron = {
+            type = "app";
+            program = "${self.packages.${system}.picogron}/bin/picogron";
+          };
         in {
+          inherit picogron;
+          default = picogron;
+        };
+
         devShells.default = pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.zigpkgs.master-2024-04-07
+        nativeBuildInputs = nativeBuildInputs;
+        buildInputs = [
           zls
+          pkgs.zon2nix
           pkgs.jq
           pkgs.fd
 
