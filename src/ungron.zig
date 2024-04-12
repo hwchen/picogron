d9afab4914aa020bfacb8120fc0f4a6bbead72da
Walther Chen <walther.chen@gmail.com>
2024-04-12 06:28:14 -0400

    ungron: parse whole doc
    - read doc at once, instead of by line
    - remove write to jsonstream (because it doesn't let you write values
      byte by byte)
    - removes need for huge buffer for line (which needed to handle possibly
      large values). Buffer for field is smaller, as we assume field names
      are not that big.
    - does not handle commas properly yet.

--- src/ungron.zig
+++ src/ungron.zig
@@ -2,79 +2,137 @@ const std = @import("std");
 const builtin = @import("builtin");
 const mem = std.mem;
 const math = std.math;
-const parse_gron = @import("parse_gron.zig");
 
 pub fn ungron(rdr: anytype, wtr: anytype) !void {
     var br = std.io.bufferedReaderSize(4096 * 8, rdr);
     const input = br.reader();
     var bw = std.io.bufferedWriter(wtr);
     const stdout = bw.writer();
-    var jws = std.json.writeStream(stdout, .{});
 
     // tracks nesting levels of array and object
     // Currently uses difference in nesting level between two paths to know
     // how far back to pop.
-    var stack = try std.BoundedArray(parse_gron.LastField, 1024).init(0);
+    var stack = try std.BoundedArray(LastField, 1024).init(0);
     try stack.append(.root);
 
     var prev_path_nest: u32 = 0;
-    var line_buf: [4096 * 1000]u8 = undefined;
-    while (try input.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
-        const path_info = parse_gron.parseLine(line);
-        const val = path_info.value;
-        const val_is_obj = val[0] == '{';
-        const val_is_arr = val[0] == '[';
-        const last_field = path_info.last_field;
-
-        // Try to end objects and arrays
-        if (path_info.nest < prev_path_nest) {
-            for (0..prev_path_nest - path_info.nest) |_| {
-                const last_nest = stack.pop();
-                switch (last_nest) {
-                    .array => try jws.endArray(),
-                    .object, .object_in_brackets => try jws.endObject(),
-                    .root => unreachable,
+    var curr_path_nest: u32 = 0;
+    var last_field: LastField = .root;
+    var last_field_str = try std.BoundedArray(u8, 256).init(0);
+    while (true) {
+        const c = input.readByte() catch break;
+        switch (c) {
+            ' ' => { // value start, processes to semicolon eol
+                // Try to end objects and arrays
+                if (curr_path_nest < prev_path_nest) {
+                    for (0..prev_path_nest - curr_path_nest) |_| {
+                        const last_nest = stack.pop();
+                        switch (last_nest) {
+                            .array => try stdout.writeByte(']'),
+                            .object => try stdout.writeByte('}'),
+                            .root => unreachable,
+                        }
+                    }
                 }
-            }
-        }
-        prev_path_nest = path_info.nest;
+                prev_path_nest = curr_path_nest;
+                curr_path_nest = 0;
 
-        // flushing more often helps with debugging
-        if (builtin.mode == .Debug) {
-            try bw.flush();
-        }
+                // flushing more often helps with debugging
+                if (builtin.mode == .Debug) {
+                    try bw.flush();
+                }
 
-        // write fields and values
-        switch (last_field) {
-            .object => try jws.objectField(path_info.last_field_str),
-            .object_in_brackets => try jws.objectFieldRaw(path_info.last_field_str),
-            else => {},
-        }
-        if (val_is_obj) {
-            try jws.beginObject();
-            try stack.append(.object);
-        } else if (val_is_arr) {
-            try jws.beginArray();
-            try stack.append(.array);
-        } else {
-            // if string, print exact, if using write will escape escape chars
-            // this also prints numbers, null, bools exactly as they were read.
-            try jws.print("{s}", .{val});
-        }
-        // flushing more often helps with debugging
-        if (builtin.mode == .Debug) {
-            try bw.flush();
+                // write fields and values
+                switch (last_field) {
+                    .object => {
+                        // expects field name without quotes
+                        try stdout.writeByte('"');
+                        _ = try stdout.write(last_field_str.slice());
+                        _ = try stdout.write("\":");
+                    },
+                    else => {},
+                }
+
+                // write value
+                std.debug.assert(try input.readByte() == '=');
+                std.debug.assert(try input.readByte() == ' ');
+                const val_start = try input.readByte();
+                switch (val_start) {
+                    '{' => {
+                        try stdout.writeByte('{');
+                        try stack.append(.object);
+                    },
+                    '[' => {
+                        try stdout.writeByte('[');
+                        try stack.append(.array);
+                    },
+                    else => {
+                        try stdout.writeByte(val_start);
+                        while (true) {
+                            const val_c = try input.readByte();
+                            switch (val_c) {
+                                ';' => {
+                                    // TODO fix trailing commas
+                                    try stdout.writeByte(',');
+                                    break;
+                                },
+                                else => try stdout.writeByte(val_c),
+                            }
+                        }
+                    },
+                }
+                // flushing more often helps with debugging
+                if (builtin.mode == .Debug) {
+                    try bw.flush();
+                }
+            },
+            '\n' => continue,
+            '.' => {
+                last_field = .object;
+                try last_field_str.resize(0);
+                curr_path_nest += 1;
+            },
+            '[' => { // need to handle object in brackets here, as it may contain chars we switch in the main loop.
+                if (try input.readByte() == '"') {
+                    last_field = .object;
+                    try last_field_str.resize(0);
+                    try last_field_str.append('"');
+                    while (true) {
+                        const c_in_quotes = try input.readByte();
+                        switch (c_in_quotes) {
+                            '\\' => {
+                                try last_field_str.append('\\');
+                                try last_field_str.append(try input.readByte());
+                            },
+                            '"' => break,
+                            else => try last_field_str.append(c_in_quotes),
+                        }
+                    }
+                    std.debug.assert(try input.readByte() == ']');
+                } else {
+                    last_field = .array;
+                    try last_field_str.resize(0); // not needed?
+                }
+                curr_path_nest += 1;
+            },
+            ']' => {},
+            else => try last_field_str.append(c),
         }
     }
-
     // Close any remaining objects or arrays
     while (stack.popOrNull()) |item| {
         switch (item) {
-            .array => try jws.endArray(),
-            .object, .object_in_brackets => try jws.endObject(),
+            .array => try stdout.writeByte(']'),
+            .object => try stdout.writeByte('}'),
             .root => {},
         }
     }
     _ = try bw.write("\n");
     try bw.flush();
 }
+
+const LastField = enum {
+    root,
+    array,
+    object,
+};
