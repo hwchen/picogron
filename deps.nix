1680b6ca600309a5e65c91e2fe29991628029d02
Walther Chen <walther.chen@gmail.com>
2024-07-27 10:39:17 -0400

    update zon2nix > deps.nix

--- deps.nix
+++ deps.nix
@@ -7,7 +7,7 @@ linkFarm "zig-packages" [
     name = "1220db88c1ba7cfddb6915b61d9cf9ae58ced88d74ad4efbfff1d1dc95236086c251";
     path = fetchzip {
       url = "https://codeberg.org/dude_the_builder/zg/archive/master.tar.gz";
-      hash = "sha256-upZhUnBArdmAOIy9eECgRcVMwzc5kzeyS0ymhPlfNIU=";
+      hash = "sha256-1dtXCXCsnQLWzfXaPjMUV+9+ZAwCwr2uaLoGaJobr/E=";
     };
   }
 ]
