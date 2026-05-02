cd2b3ce81545064fe5b3f5ebbfbd9f413299d9e7
walther chen <walther.chen@gmail.com>
2026-05-02 10:26:58 +0700

    gron read line to buf

--- src/gron_stream.zig
+++ src/gron_stream.zig
@@ -5,14 +5,12 @@ const gron = @import("gron.zig");
 pub fn gronStream(rdr: anytype, wtr: anytype) !void {
     var br = std.io.bufferedReader(rdr);
     const input = br.reader();
-    var line_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const line_alloc = line_arena.allocator();
+    var line_buf: [4096 * 8]u8 = undefined;
 
     _ = try wtr.write("json = [];\n");
 
     var line_idx: usize = 0;
-    while (try input.readUntilDelimiterOrEofAlloc(line_alloc, '\n', math.maxInt(u32))) |line| {
-        defer _ = line_arena.reset(.retain_capacity);
+    while (try input.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
         var line_stream = std.io.fixedBufferStream(line);
         try gron.gron(line_stream.reader(), wtr, .{ .line_idx = line_idx });
         line_idx += 1;
